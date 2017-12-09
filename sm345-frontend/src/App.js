23eaba5532520048d5588132712a6b0e7860ccde
chudee <geonwoo.chu@gmail.com>
2017-12-09 14:01:26 +0900

    초기 설정중

--- sm345-frontend/src/App.js
+++ sm345-frontend/src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import {  } from 'mobx';
+// import { Home } from 'routes';
+// import Header from 'components/Header';
+// import DrawerMenu from 'components/DrawerMenu';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <MuiThemeProvider>
+        <Router>
+          <div>
+            {/* <Header/> */}
+            {/* <DrawerMenu/> */}
+            {/* <Route path="/" component={Home} /> */}
+          </div>
+        </Router>
+    </MuiThemeProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ sm345-frontend/src/models/room.js
@@ -0,0 +1,15 @@
+// import { observable, computed, action } from "mobx";
+
+// export default class RoomModel {
+//     @observable todos = [];
+  
+//     @computed
+//     get unfinishedTodoCount() {
+//       return this.todos.filter(todo => !todo.finished).length;
+//     }
+  
+//     @action
+//     addTodo(title) {
+//       this.todos.push(new TodoModel(title));
+//     }
+//   }
